23b9cd58cbe96206dd16c9cf351de5b952bfe897
changer0 <zhanglulu1007@foxmail.com>
2016-12-20 11:09:47 +0800

    Initial commit

---
+++ app/src/main/java/com/lulu/tuyou/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.lulu.tuyou.activity;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lulu.tuyou.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+    }
+}
