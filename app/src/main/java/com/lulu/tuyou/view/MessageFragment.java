1864da84afb4228cb916902d3cd4f796a347928b
changer0 <zhanglulu1007@foxmail.com>
2017-01-21 23:13:21 +0800

    添加聊天界面Demo

--- app/src/main/java/com/lulu/tuyou/view/MessageFragment.java
+++ app/src/main/java/com/lulu/tuyou/view/MessageFragment.java
@@ -2,6 +2,8 @@ package com.lulu.tuyou.view;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,8 +11,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.lulu.tuyou.R;
+import com.lulu.tuyou.databinding.FragmentMessageBinding;
+
+import org.kymjs.chat.ChatActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,69 +41,16 @@ public class MessageFragment extends Fragment {
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        //Log.d("lulu", "MessageFragment-onAttach  ");
-    }
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //Log.d("lulu", "MessageFragment-onCreate  ");
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //Log.d("lulu", "MessageFragment-onCreateView  ");
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        //Log.d("lulu", "MessageFragment-onActivityCreated  ");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        //Log.d("lulu", "MessageFragment-onStart  ");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        //Log.d("lulu", "MessageFragment-onResume  ");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        //Log.d("lulu", "MessageFragment-onPause  ");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        //Log.d("lulu", "MessageFragment-onStop  ");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        //Log.d("lulu", "MessageFragment-onDestroyView  ");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //Log.d("lulu", "MessageFragment-onDestroy  ");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        //Log.d("lulu", "MessageFragment-onDetach  ");
+        FragmentMessageBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_message, container, false);
+        binding.msgJump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "点击", Toast.LENGTH_SHORT).show();
+                getContext().startActivity(new Intent(getContext(), ChatActivity.class));
+            }
+        });
+        return binding.getRoot();
     }
 }
