475b34c77668da12f060a8439480068e3d42345a
zhanglulu <zhanglulu@foxmail.com>
2017-05-07 20:48:44 +0800

    修复上传

---
+++ app/src/main/java/com/lulu/tuyou/view/MyGridView.java
@@ -0,0 +1,26 @@
+package com.lulu.tuyou.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by lulu on 2017/5/7.
+ *
+ */
+
+public class MyGridView extends GridView {
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int spec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, spec);
+    }
+}
